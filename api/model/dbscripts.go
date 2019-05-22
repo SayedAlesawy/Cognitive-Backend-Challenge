0f593a85523938d08e75ce38ebd0debdd44920a9
SayedAlesawy <sayedalesawy14@gmail.com>
2019-05-22 20:20:03 +0200

    Added SQL for all CRUD operations

--- api/model/dbscripts.go
+++ api/model/dbscripts.go
@@ -4,6 +4,7 @@ package campaign
 const SQLCreateCampaignsTable string = `
 	CREATE TABLE campaigns (
 		id SERIAL PRIMARY KEY,
+		ID varchar(60) UNIQUE NOT NULL,
 		Name varchar(60) NOT NULL,
 		Country varchar(60) NOT NULL,
 		Budget float NOT NULL, 
@@ -16,4 +17,32 @@ const SQLCreateCampaignsTable string = `
 // SQLDropCampaignsTable SQL to drop the campaings table
 const SQLDropCampaignsTable string = `
 	DROP TABLE IF EXISTS campaigns;
+`
+
+// Defining all the CRUD operations from the campaign resource
+// CRUD => Create - Read - Update - Delete
+
+// SQLCreateCampaign SQL to create a new campaign record
+const SQLCreateCampaign string = `
+	INSERT INTO campaigns (ID, Name, Country, Budget, Goal, Category, URL)
+	VALUES ($1, $2, $3, $4, $5, $6, $7)
+`
+
+// SQLReadCampaing SQL to read a campaign record
+const SQLReadCampaing string = `
+	SELECT * FROM campaigns
+	WHERE ID = $1
+`
+
+// SQLUpdateCampaign SQL to update a campaign record
+const SQLUpdateCampaign string = `
+	UPDATE campaigns
+	SET Name = $1, Country = $2, Budget = $3, Goal = $4, Category = $5, URL = $6
+	WHERE ID = $7
+`
+
+// SQLDeleteCampaign SQL to delete a campaign record
+const SQLDeleteCampaign string = `
+	DELETE FROM campaigns
+	where ID = $1
 `
--- api/model/model.go
+++ api/model/model.go
@@ -2,6 +2,7 @@ package campaign
 
 // Campaign Represents the campaign resource
 type Campaign struct {
+	ID       string  //Unique identifier for the Campaign record
 	Name     string  //The Campaign name
 	Country  string  //The country at which the Campaign orginates
 	Budget   float32 //The budget allocated to the Campaign
