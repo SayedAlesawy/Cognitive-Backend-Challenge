68e65da4638a18d6369d770cbad8dcb8d8529a18
SayedAlesawy <sayedalesawy14@gmail.com>
2019-05-22 21:20:34 +0200

    Added json tags to model attributes

--- api/model/model.go
+++ api/model/model.go
@@ -2,11 +2,11 @@ package campaign
 
 // Campaign Represents the campaign resource
 type Campaign struct {
-	ID       string  //Unique identifier for the Campaign record
-	Name     string  //The Campaign name
-	Country  string  //The country at which the Campaign orginates
-	Budget   float32 //The budget allocated to the Campaign
-	Goal     string  //The market goal of the Campaign
-	Category string  //Fetched from the category extraction service if not provided
-	URL      string  //Used to fetched the category from the extraction service if not provided
+	ID       string  `json:"id,omitempty"`       //Unique identifier for the Campaign record
+	Name     string  `json:"name,omitempty"`     //The Campaign name
+	Country  string  `json:"country,omitempty"`  //The country at which the Campaign orginates
+	Budget   float32 `json:"budget,omitempty"`   //The budget allocated to the Campaign
+	Goal     string  `json:"goal,omitempty"`     //The market goal of the Campaign
+	Category string  `json:"category,omitempty"` //Fetched from the category extraction service if not provided
+	URL      string  `json:"url,omitempty"`      //Used to fetched the category from the extraction service if not provided
 }
